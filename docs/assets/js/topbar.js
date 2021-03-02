b52b86c46675bc5267fe854cffbde0ec1d47a2d4
DragonKnightOfBreeze <dk_breeze@qq.com>
2021-03-02 09:53:12 +0800

    添加文档下载地址

--- docs/assets/js/topbar.js
+++ docs/assets/js/topbar.js
@@ -1,30 +1,30 @@
 window.$docsify.topbar = {
-  downloadUrl: "javascript:void(0)", //TODO
-    downloadText: {
+  downloadUrl: "https://hotpu-1257128263.cos.ap-shanghai.myqcloud.com/%E5%AF%B9%E5%A4%96%E5%BC%80%E6%94%BE%E4%B8%8B%E8%BD%BD%E6%96%87%E6%A1%A3/%E7%83%AD%E7%92%9E%E6%95%B0%E6%8D%AE%E5%BA%93HotDB%E4%BD%BF%E7%94%A8%E6%89%8B%E5%86%8C-2.5.6--V2.0.rar",
+  downloadText: {
     "/zh/": "<i class='fa fa-download'></i> 下载文档",
-      "/en/": "<i class='fa fa-download'></i> Download Document"
+    "/en/": "<i class='fa fa-download'></i> Download Document"
   },
   downloadTitle: {
     "/zh/": "下载整合的PDF文档",
-      "/en/": "Download integrated pdf document"
+    "/en/": "Download integrated pdf document"
   },
   editUrl: `${repositoryUrl}/blob/master/docs`,
-    editText: {
+  editText: {
     "/zh/": "<i class='fa fa-edit'></i> 编辑文档",
-      "/en/": "<i class='fa fa-edit'></i> Edit Document"
+    "/en/": "<i class='fa fa-edit'></i> Edit Document"
   },
   editTitle: {
     "/zh/": "在Github上编辑文档",
-      "/en/": "Edit document on Github"
+    "/en/": "Edit document on Github"
   },
   issuesUrl: `${repositoryUrl}/issues`,
     issuesText: {
     "/zh/": "<i class='fa fa-comment'></i> 反馈问题",
-      "/en/": "<i class='fa fa-comment'></i> Report Issues"
+    "/en/": "<i class='fa fa-comment'></i> Report Issues"
   },
   issuesTitle: {
     "/zh/": "在Github上反馈问题",
-      "/en/": "Report Issues on Github"
+    "/en/": "Report Issues on Github"
   }
 }
 
